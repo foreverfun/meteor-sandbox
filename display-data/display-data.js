8f70303ed4eba45b3d2137614703e8c07e3fb1c0
cj lin <foreverfun7@gmail.com>
2015-03-06 09:03:23 -0700

    version 1.00.02

---
+++ display-data/display-data.js
@@ -0,0 +1,66 @@
+Members = new Mongo.Collection("members");
+//Members = new Meteor.Collection("members");
+
+if (Meteor.isClient) {
+  // // counter starts at 0
+  // Session.setDefault('counter', 0);
+
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
+
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
+  
+  // Template.body.helpers({
+  //   members: [
+  //     {name: "John"},
+  //     {name: "Mike"},
+  //     {name: "Andy"}
+  //   ]
+  // });
+   
+  Template.body.helpers({
+    members: function() {
+      //Members.insert({name:"cj"});
+      return Members.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-member": function(event) {
+      var name = event.target.name.value;
+      
+      Members.insert({
+          name: name
+      });
+      
+      event.target.name.value = "";
+
+      return false;
+    },
+
+    "click .delete": function(event) {
+      // console.log($(event.target).closest('li').attr('data-id'));
+      // console.log(Blaze.getData(event.target));
+      // Blaze is meteor's view template
+      var d = Blaze.getData(event.target);
+      Members.remove({_id:d._id});
+      return false;
+    }
+  
+  });
+
+}
+
+if (Meteor.isServer) {
+  // Meteor.startup(function () {
+  //   // code to run on server at startup
+  // });
+}
